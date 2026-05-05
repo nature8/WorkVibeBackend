b6a9ceb1285b0cbda983a58d5fa3917c978acfee
Prakruti Tailor <50707451+nature8@users.noreply.github.com>
2026-05-05 17:17:19 +0530

    Initial commit 2

--- backend/src/main/java/com/repository/JobRepository.java
+++ backend/src/main/java/com/repository/JobRepository.java
@@ -7,5 +7,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+	List<Job> findByLocation(String location);
+    List<Job> findByExperience(String experience);
+    List<Job> findByTitle(String title);
+
+    List<Job> findByLanguagesContainingIgnoreCase(String language);
+    List<Job> findByTitleContainingIgnoreCase(String keyword);
+    List<Job> findByLocationContainingIgnoreCase(String location);
 }
 
